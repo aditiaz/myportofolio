e429ba5d8f4e70c4718ddd167fb79f22a5d69a2b
aditiaz <aditiaan96a@gmail.com>
2023-03-09 11:22:49 +0700

    fixin

--- pages/housy.jsx
+++ pages/housy.jsx
@@ -34,7 +34,7 @@ const housy = () => {
           <button className="px-8 py-2 mt-4">Front-end code </button>
           <button className="px-8 py-2 mt-4 ml-8">Back-end code </button>
         </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xlgit p-4">
           <div className="p-2">
             <p className="text-center font-bold pb-2">Technologies</p>
             <div className="grid grid-cols-3 md:grid-cols-1">
